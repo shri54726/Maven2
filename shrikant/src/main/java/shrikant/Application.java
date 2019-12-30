bff773857b9601761bfb01141eebf4d2e54bb8bb
Shri54726 <shri.korat@gmail.com>
2019-12-30 15:29:21 +0530

    Maven2 push

---
+++ shrikant/src/main/java/shrikant/Application.java
@@ -0,0 +1,10 @@
+package shrikant;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Shrikant");
+		System.out.println("Kasa aahes tu...?");
+	}
+
+}
